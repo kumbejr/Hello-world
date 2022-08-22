e034765f2f09b8a0ed8afda717bdf190a37a1e06
MohamedAhmed <mohamedmekka@hotmail.com>
2022-08-22 18:04:46 +0200

    va troru bre 2

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("hello world change 2");
+        System.out.println("hello world change 3");
     }
 }
